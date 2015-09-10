62aa484f3e6f5e0c25ab98d105b584b1c2326f81
Dmitry Zhiltsov <dzhiltsov@me.com>
2015-09-10 13:38:06 +0300

    Add GustBookMessagesManager

--- guest_book/admin.py
+++ guest_book/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# coding=utf-8
+__author__ = "Dmitry Zhiltsov"
+__copyright__ = "Copyright 2015, Dmitry Zhiltsov"
 
-# Register your models here.
+from django.contrib.auth.admin import admin
+
+from guest_book.models import GuestBook, GuestBookMessages
+
+
+class GuestBookAdmin(admin.ModelAdmin):
+    pass
+
+
+class GuestBookMessagesAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
+admin.site.register(GuestBookMessages, GuestBookMessagesAdmin)
--- guest_book/models.py
+++ guest_book/models.py
@@ -8,6 +8,13 @@ from django.conf import settings
 from gbfuser.models import GBFUser
 
 
+class GuestBookMessageManager(models.Manager):
+    def create_message(self, message_data):
+        is_visible = not self.guest_book.is_moderated
+        gb_message = self.create(is_visible=is_visible, **message_data)
+        return gb_message
+
+
 class GuestBook(models.Model):
     name = models.CharField('Guest Book Name', max_length=250)
     slug = models.SlugField('Guest Book Slug', max_length=50)
@@ -24,13 +31,19 @@ class GuestBook(models.Model):
 
 
 class GuestBookMessages(models.Model):
+    objects = GuestBookMessageManager()
+
     guest_book = models.ForeignKey(GuestBook)
-    message = models.TextField('Message') #TODO Align limit
+    message = models.TextField('Message')  # TODO Align limit
     author = models.ForeignKey(GBFUser)
     time_of_creation = models.DateTimeField(auto_now_add=True)
     is_visible = models.BooleanField()
-    time_of_moderate = models.DateTimeField(blank=True)
+    time_of_moderate = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         verbose_name = 'Guest Book Message'
         verbose_name_plural = 'Guest Books Messages'
+
+    def __str__(self):
+        return "Author: {} Guest Book: {} Guest Book Owner: {}".format(self.guest_book.name, self.guest_book.owner,
+                                                                       self.author)
